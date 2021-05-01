b22f675b43d00d70eeb7421e330d26ffaea32946
Fredrik Wibbels <fredrik.wibbels@avensia.com>
2021-05-01 11:36:34 +0200

    Add CartExtension with AddPromotions to cart

---
+++ PromotionEngine.Tests/CartTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Models;
+
+namespace PromotionEngine.Tests
+{
+    [TestClass]
+    public class CartTests
+    {
+        private static IList<IPromotion> promotions = new List<IPromotion>
+        {
+            new BuyXItemsForFixedPricePromtion("A", 130, 3),
+            new BuyXItemsForFixedPricePromtion("B", 45, 2)
+        };
+
+        [TestMethod]
+        public void TestTotalSum()
+        {
+            //Arrange
+            var promo = promotions.First();
+            var cart = new Cart
+            {
+                LineItems = new List<LineItem>
+                {
+                    new LineItem
+                    {
+                        Quantity = 1,
+                        ArticleNumber = "A",
+                        Price = 50
+                    },
+                    new LineItem
+                    {
+                        Quantity = 1,
+                        ArticleNumber = "B",
+                        Price = 30
+                    },
+                    new LineItem
+                    {
+                        Quantity = 1,
+                        ArticleNumber = "C",
+                        Price = 20
+                    },
+                }
+            };
+
+            //Act
+            cart.AddDiscountsToCart(promotions);
+
+            //Assert
+            Assert.AreEqual(100, cart.TotalSum);
+        }
+    }
+}
---
+++ Src/PromotionEngine/CartExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Models;
+
+namespace PromotionEngine
+{
+    public static class CartExtensions
+    {
+        public static void AddDiscountsToCart(this Cart cart, IList<IPromotion> promotions)
+        {
+            do
+            {
+                foreach (var promo in promotions)
+                {
+                    promo.AddDiscountToCart(cart);
+                }
+            } while (promotions.Any(p => p.IsDiscountValidForCart(cart)));
+        }
+    }
+}
